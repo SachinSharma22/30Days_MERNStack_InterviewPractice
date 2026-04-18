e82ca21ac6a87a0a5b84981d33e942a17b74eeb9
SachinSharma22 <s830sharma@gmail.com>
2026-04-18 10:57:46 +0530

    copying one object into other obj and change its value

--- 1-MernPractice/index.js
+++ 1-MernPractice/index.js
@@ -1,7 +1,9 @@
-console.log("1");
-setTimeout(() => console.log("2"),0);
-Promise.resolve().then(()=> console.log("5")); 
-console.log("6");
+//First Question
+
+// console.log("1");
+// setTimeout(() => console.log("2"),0);
+// Promise.resolve().then(()=> console.log("5")); 
+// console.log("6");
 /**⚙️ Execution Flow (Very Important Concept)
 
 JavaScript works on:
@@ -11,3 +13,14 @@ Microtask Queue (Promises)
 Macrotask Queue (setTimeout) 
 Promises (microtasks) always execute before setTimeout (macrotasks)
 Even if setTimeout delay is 0*/
+
+//2nd Question
+
+const abc = {
+    x : 1
+}
+
+const xyz = abc;
+xyz.x = 3;
+
+console.log(abc.x);
