8faf0da3ecf8c79aa371445a8b7213253e449aac
SachinSharma22 <s830sharma@gmail.com>
2026-04-18 13:38:24 +0530

    difference between deep copy and shallow copy

--- 1-MernPractice/index.js
+++ 1-MernPractice/index.js
@@ -16,11 +16,51 @@ Even if setTimeout delay is 0*/
 
 //2nd Question
 
-const abc = {
-    x : 1
+// const abc = {
+//     x : 1
+// }
+
+// const xyz = abc;
+// xyz.x = 3;
+
+// console.log(abc.x);
+
+// Que 3. Infinite Curring
+
+// function add(a) {
+//     return function(b) {
+//         if(b!== undefined) {
+//             return add(a+b);
+//         }else{
+//             return a;
+//         }
+//     };
+// }
+
+// console.log(add(1)(2)(3)(4)());
+
+//Que 4: Difference between Deep and Shallow copy
+
+let obj1 = {
+    a: 1,
+    b: {
+        c: 2,
+    }
 }
 
-const xyz = abc;
-xyz.x = 3;
+// Shallow copy
+let obj2 = {...obj1};
+console.log(obj1);
+// console.log(obj2);
+// obj2.b.c = 4;
+// console.log(obj1);
+// console.log(obj2);
 
-console.log(abc.x);
+//Deep Copy
+
+let obj3 = JSON.parse(JSON.stringify(obj1));
+
+console.log(obj3);
+obj3.b.c = 5;
+console.log(obj1);
+console.log(obj3);
