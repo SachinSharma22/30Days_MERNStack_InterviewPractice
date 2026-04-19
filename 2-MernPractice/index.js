7b17bc3c8dcd5e91d16fcf8b31a7ed3b1f6e52d3
SachinSharma22 <s830sharma@gmail.com>
2026-04-19 23:33:57 +0530

    day 2 of practice mern interview practice

---
+++ 2-MernPractice/index.js
@@ -0,0 +1,42 @@
+// Question 1: What is array desstructueing
+/**Array destructuring is a JavaScript syntax that lets you extract values from an array and assign them to variables in one line.
+
+✅ Basic Example
+const arr = [10, 20, 30];
+
+const [a, b, c] = arr;
+
+console.log(a); // 10
+console.log(b); // 20
+console.log(c); // 30 */
+
+//Que 2 --> Example of rest Parameter
+
+//Que 3 --> 
+// const arr = [100, 10, 30, 50, 7];
+
+// const res = arr.reduce((val, data) => {
+//     return val + data;
+// })
+
+// console.log(res);
+
+// Que 4 --> Infinite curring
+
+//Que 5 --> seperate number in one array and character in different array
+// const arr = [10,'a','c', 11, 14];
+// const num = [];
+// const charac = [];
+
+// arr.forEach((val) => {
+//     if(typeof val === 'number') {
+//         num.push(val);
+//     }else if(typeof val === 'string') {
+//         charac.push(val)
+//     }
+// })
+
+// console.log(num);
+// console.log(charac);
+
+//Ques 6 --> 
